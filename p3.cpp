4969764e96f7698767891eb3059453bfb7d2df80
aziz-0110 <222degree@gmail.com>
2024-02-22 21:17:26 +0700

    menambahkan variabel mutable

--- p3.cpp
+++ p3.cpp
@@ -2,11 +2,27 @@
 
 using namespace std;
 
+// fungsi demo statik variabel
 int staticVariabel();
+// class demo mutabel variabel
+class MutableVar
+{
+public:
+   mutable int umur;
+   int noHp;
+
+   MutableVar(int umur, int noHp)
+   {
+      this->umur = umur;
+      this->noHp = noHp;
+   }
+};
+
 int main()
 {
    cout << "JENIS-JENIS VARIABEL" << endl;
    string var = "Variabel";
+   cout << endl;
 
    // statik variabel => nilai variabel akan terus disimpan meskipun variabel tersebut dideklarasikan di fungsi lain
    cout << "Statik " << var << endl;
@@ -16,6 +32,7 @@ int main()
       cout << "nilai a di fungsi main = " << a << endl;
       cout << "nilai a di fungsi statikVariabel = " << staticVariabel() << endl;
    }
+   cout << endl;
 
    cout << "Auto " << var << endl;
    // auto variabel => variabel yg secara otomatis menentukan jenis tipe datanya
@@ -23,11 +40,26 @@ int main()
    auto b{3.6};
    cout << "nilai b " << b << endl;
    // b = "aziz"  // ini akan error.
+   cout << endl;
 
    cout << "Konstan " << var << endl;
-   //
-   const int phi(3.14);
-   cont << "nilai phi " << phi << endl;
+   // konstan variabel => variabel yg nilainya tdk bisa di ganti.
+   const float phi(3.14);
+   cout << "nilai phi " << phi << endl;
+   cout << endl;
+
+   cout << "Mutabel " << var << endl;
+   // mutabel variabel => variabel secara khusus digunakan didlm class, dan fungsinya kebalikan dari variabel konstan
+   const MutableVar obj = MutableVar(21, 1234);
+   cout << "Data Sebelum Diganti" << endl;
+   cout << "noHp = " << obj.noHp << endl;
+   cout << "umur = " << obj.umur << endl;
+
+   cout << "Data Sesudah Diganti" << endl;
+   // obj.noHp = 8228; // ini akan error karena variabelnya tidak di set ke mutabel
+   obj.umur = 20;
+   cout << "noHp = " << obj.noHp << endl;
+   cout << "umur = " << obj.umur << endl;
 }
 
 int staticVariabel()
