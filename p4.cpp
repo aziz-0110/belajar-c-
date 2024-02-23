adb5d9eb1b566c5d6834ddfe70cf45196ae6831c
aziz-0110 <222degree@gmail.com>
2024-02-23 09:35:55 +0700

    menambahkan input spasi dan input banyak variabel

--- p4.cpp
+++ p4.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-   int a;
+   // int a;
 
-   cout << "masukkan nilai a = ";
-   cin >> a;
+   // cout << "masukkan nilai a = ";
+   // cin >> a;
 
-   cout << "\nnilai a = " << a << endl;
+   // cout << "\nnilai a = " << a << endl;
+
+   // string nama;
+   // cout << "nama = ";
+   // getline(cin, nama); // getline => agar cin membaca spasi
+
+   // cout << "\nhallo " << nama << endl;
+
+   int a, b, c;
+
+   cout << "masukkan nilai a, b, c = ";
+   cin >> a >> b >> c;
+
+   cout << "\nnilai a, b, c"
+        << " " << a << " " << b << " " << c << " " << endl;
 }
